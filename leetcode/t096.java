80ccd15abf1d39d0bfc50d7df636c83393f33220
telnetning <telnetning@gmail.com>
2016-07-30 11:13:30 +0800

    modify t096

--- leetcode/t096.java
+++ leetcode/t096.java
@@ -3,22 +3,17 @@ class Solution
 	static int[] resCache = new int[100];
 	static {
 		resCache[0] = 1;
-		resCache[1] = 1;
 	}
     public int numTrees(int n) 
     {
     	if(resCache[n] != 0) return resCache[n];
 
-    	int count = 0;
-		if(n <= 1) return 1;        
-
 		for(int i = 0; i < n; i++)
 		{
-			count += numTrees(i) * numTrees(n - 1 - i);	
+			resCache[n] += numTrees(i) * numTrees(n - 1 - i);	
 		}
-
-		resCache[n] = count;
-		return count;
+        
+        return resCache[n];
     }
 }
 
